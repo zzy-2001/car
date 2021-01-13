742735744c4e86831143ffde6dcafff042a1c962
zzy <1927818896@qq.com>
2021-01-13 15:24:25 +0800

    车辆信息  车型管理 新增 修改 调查  zzy

--- api/src/main/java/com/jk/service/Hystrix.java
+++ api/src/main/java/com/jk/service/Hystrix.java
@@ -1,7 +1,41 @@
 package com.jk.service;
 
+import com.jk.pojo.CarBean;
+import com.jk.pojo.TreeBean;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Component
 public class Hystrix implements CarService{
+    @Override
+    public List<TreeBean> findtree() {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, Object> findCar(Integer page, Integer rows, CarBean bean) {
+        return null;
+    }
+
+    @Override
+    public List<CarBean> findType() {
+        return null;
+    }
+
+    @Override
+    public List<CarBean> findColor() {
+        return null;
+    }
+
+    @Override
+    public void addCar(CarBean bean) {
+
+    }
+
+    @Override
+    public CarBean findById(Integer id) {
+        return null;
+    }
 }
